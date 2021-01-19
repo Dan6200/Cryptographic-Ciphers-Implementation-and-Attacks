1d3dad5b7392ef1d8dc605cf0d3e190715ed59ce
Dan6200 <57610029+Dan6200@users.noreply.github.com>
2021-01-19 00:41:40 -0500

    Update main.cpp

--- Substitution-Ciphers/Sub-Cipher/main.cpp
+++ Substitution-Ciphers/Sub-Cipher/main.cpp
@@ -6,7 +6,7 @@ int main() {
     char usrInput[100000];
     std::ofstream output;
     output.open("cipher-text.txt");
-    Sub_Ciph cipher;
+    Shift_Ciph cipher;
     std::cout << "This cipher is more secure than the Caesar-cipher, (not that secure, if you are clever)\nEnter text to be encrypted (to quit enter \"|\" character):\n\n";
     std::cin.get(usrInput, 100000, '|');
     std::cout << '\n';
